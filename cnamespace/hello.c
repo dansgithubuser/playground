e367193a548ec573aae3db00ed30e4c8a53bf17e
Dan <dansonlinepresence@gmail.com>
2015-08-09 14:20:25 -0400

    mimic namespacing with c preprocessor

---
+++ cnamespace/hello.c
@@ -0,0 +1,11 @@
+//main file
+#include <stdio.h>
+
+void NAMESPACE(hello)(){
+	printf("Hello, world!\n");
+}
+
+int main(){
+	LOL_hello();
+	return 0;
+}
---
+++ cnamespace/make.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('gcc -DNAMESPACE(X)=LOL_##X hello.c')
