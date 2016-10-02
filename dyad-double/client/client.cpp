cc51672c0660d690f5da26d2420994aef011b29b
Dan <dansonlinepresence@gmail.com>
2016-10-01 21:27:12 -0400

    Revert "dyad double linking -- logging"
    This reverts commit 99e2e9b336efbcd519b4f2d555cca4273970d6af.

--- dyad-double/client/client.cpp
+++ dyad-double/client/client.cpp
@@ -3,8 +3,6 @@
 #include <dryad.hpp>
 
 #include <iostream>
-#include <thread>
-#include <sstream>
 
 static void receiver(const std::vector<uint8_t>& data){
 	std::cout<<"client rx: ";
@@ -13,33 +11,20 @@ static void receiver(const std::vector<uint8_t>& data){
 }
 
 Client::Client(){
-	printf("Client::Client 1\n");
 	dryad_client::start();
-	printf("Client::Client 2\n");
 	_client=new dryad_client::Client("127.0.0.1", 9089, receiver);
-	printf("Client::Client 3\n");
 	_quit=false;
 	_thread=std::thread([this](){
-		std::stringstream ss;
-		ss<<std::this_thread::get_id();
-		printf("client thread %s\n", ss.str().c_str());
 		while(!_quit){
 			std::this_thread::sleep_for(std::chrono::seconds(1));
-			printf("client sending\n");
 			((dryad_client::Client*)_client)->send({'a', 's', 'd', 'f'});
 		}
-		printf("client thread done\n");
 	});
-	printf("Client::Client 10\n");
 }
 
 Client::~Client(){
 	_quit=true;
-	printf("client quitting\n");
 	_thread.join();
-	printf("client joined\n");
 	delete _client;
-	printf("client deleted\n");
 	dryad_client::finish();
-	printf("client finished\n");
 }
--- dyad-dryad/dryad.cpp
+++ dyad-dryad/dryad.cpp
@@ -7,30 +7,7 @@
 #include <mutex>
 #include <thread>
 
-#include <iostream>
-#include <sstream>
-
 namespace DRYAD_NAMESPACE{
-
-class Mutex{
-	public:
-		void lock(){
-			std::stringstream ss;
-			ss<<std::this_thread::get_id();
-			std::string s=ss.str();
-			printf("%s locking %p\n", s.c_str(), this);
-			_mutex.lock();
-			printf("%s locked %p\n", s.c_str(), this);
-		}
-		void unlock(){
-			std::stringstream ss;
-			ss<<std::this_thread::get_id();
-			printf("%s unlocking %p\n", ss.str().c_str(), this);
-			_mutex.unlock();
-		}
-	private:
-		std::recursive_mutex _mutex;
-};
 
 static void onAccept(dyad_Event* e);
 static void onData(dyad_Event* e);
@@ -38,44 +15,30 @@ static void onData(dyad_Event* e);
 class Boss{
 	public:
 		Boss(){
-			printf("dryad boss %p %s\n", this, DRYAD_PREFIX);
 			dyad_init();
-			dyad_setUpdateTimeout(1);
+			dyad_setUpdateTimeout(1e-3);
 			_quit=false;
-			_thread=std::thread([this](){
-				std::stringstream ss;
-				ss<<std::this_thread::get_id();
-				printf("dyad_update thread %s\n", ss.str().c_str());
-				while(!_quit){
-					_mutex.lock();
-					dyad_update();
-					_mutex.unlock();
-				}
-			});
-			printf("dryad boss constructor returning\n");
+			_thread=std::thread([this](){ while(!_quit){
+				_mutex.lock();
+				dyad_update();
+				_mutex.unlock();
+			} });
 		}
 		~Boss(){
 			_quit=true;
 			_thread.join();
 		}
 		dyad_Stream* server(std::string host, int port, Receiver receiver){
-			printf("dryad boss %p server 1\n", this);
 			_mutex.lock();
-			printf("dryad boss %p server 2\n", this);
 			auto stream=dyad_newStream();
-			printf("dryad boss %p server 3\n", this);
 			_receivers[stream]=receiver;
 			dyad_addListener(stream, DYAD_EVENT_ACCEPT, onAccept, NULL);
 			dyad_listenEx(stream, host.c_str(), port, 511);
-			printf("dryad boss %p server 4\n", this);
 			_mutex.unlock();
-			printf("dryad boss %p server 5\n", this);
 			return stream;
 		}
 		dyad_Stream* client(std::string host, int port, Receiver receiver){
-			printf("dryad boss %p client locking mutex\n", this);
 			_mutex.lock();
-			printf("dryad boss %p client locked mutex\n", this);
 			auto stream=dyad_newStream();
 			_receivers[stream]=receiver;
 			dyad_addListener(stream, DYAD_EVENT_DATA, onData, NULL);
@@ -122,7 +85,7 @@ class Boss{
 	private:
 		std::atomic<bool> _quit;
 		std::thread _thread;
-		Mutex _mutex;
+		std::recursive_mutex _mutex;
 		std::map<dyad_Stream*, Receiver> _receivers;
 		std::map<dyad_Stream*, std::vector<dyad_Stream*>> _accepted;
 };
@@ -132,10 +95,7 @@ static Boss* fBoss;
 static void onAccept(dyad_Event* e){ fBoss->accept(e); }
 static void onData(dyad_Event* e){ fBoss->receive(e); }
 
-void start(){
-	fBoss=new Boss;
-	printf("dryad boss pointer %p %p %s\n", &fBoss, fBoss, DRYAD_PREFIX);
-}
+void start(){ fBoss=new Boss; }
 void finish(){ delete fBoss; }
 
 Server::Server(std::string host, int port, Receiver receiver){
