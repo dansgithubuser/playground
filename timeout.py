5c03e2e54ba81136c0d8bbb47467466f3b11fdc5
deisner <deisner@bionym.com>
2017-01-18 16:53:59 -0500

    (timeout.py) more logging

--- timeout.py
+++ timeout.py
@@ -12,9 +12,21 @@ import os, subprocess, sys, time
 
 t=time.time()
 p=subprocess.Popen(['/usr/bin/git']+sys.argv[1:])
+def unique_open(pid):
+	for i in range(1000):
+		p='{}-{}'.format(os.path.join(os.path.expanduser('~'), 'timeout', str(pid)), i)
+		if not os.path.exists(p): break
+		i+=1
+	return open(p, 'a')
+file=unique_open(p.pid)
+file.write(timestamp.timestamp()+' '+' '.join(sys.argv)+'\n')
 d=0.001
 while time.time()<t+300:
-	if p.poll()!=None: import sys; sys.exit(p.returncode)
+	if p.poll()!=None:
+		file.write('returncode {}\n'.format(p.returncode))
+		file.close()
+		import sys
+		sys.exit(p.returncode)
 	time.sleep(d)
 	d=min(d*2, 60)
 with open(os.path.join(os.path.expanduser('~'), 'timeout.txt'), 'a') as file:
@@ -24,4 +36,5 @@ with open(os.path.join(os.path.expanduser('~'), 'timeout.txt'), 'a') as file:
 	file.write('pid: {}\n'.format(p.pid))
 p.kill()
 sys.stderr.write('timeout\n')
+file.close()
 sys.exit(1)
