df6b940f6f788f82f35b2fdb06f335459f12c066
deisner <deisner@bionym.com>
2017-01-19 15:49:25 -0500

    unambiguous option for python timestamp

--- timestamp.py
+++ timestamp.py
@@ -1,6 +1,8 @@
-def timestamp():
+def timestamp(ambiguous=True):
 	import datetime
-	return '{:%Y-%m-%d %H:%M:%S.%f}'.format(datetime.datetime.now())
+	format='{:%Y-%m'
+	if not ambiguous: format+='-%b'
+	format+='-%d %H:%M:%S.%f}'
+	return format.format(datetime.datetime.now()).lower()
 
 if __name__=='__main__': print(timestamp())
-
