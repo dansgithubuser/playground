11bd1afc1c9264753a58e588c340206597d68255
Dan <dansonlinepresence@gmail.com>
2019-05-29 10:23:44 -0400

    python timestamp easier copyability

--- python/timestamp.py
+++ python/timestamp.py
@@ -1,8 +1,12 @@
-def timestamp(ambiguous=True):
-	import datetime
-	format='{:%Y-%m'
-	if not ambiguous: format+='-%b'
-	format+='-%d %H:%M:%S.%f}'
-	return format.format(datetime.datetime.now()).lower()
+def timestamp():
+    import datetime
+    return '{:%Y-%m-%d %H:%M:%S.%f}'.format(datetime.datetime.now())
 
-if __name__=='__main__': print(timestamp())
+def timestamp(ambiguous=True):
+    import datetime
+    format = '{:%Y-%m'
+    if not ambiguous: format += '-%b'
+    format += '-%d %H:%M:%S.%f}'
+    return format.format(datetime.datetime.now()).lower()
+
+if __name__ == '__main__': print(timestamp())
