25e053661527949e6cb41cdbd098565717397c1b
Dan <daniel@x-matik.com>
2020-04-15 14:18:47 -0400

    grpc - server -> client streaming

--- interfaces/grpc/rs-client/src/main.rs
+++ interfaces/grpc/rs-client/src/main.rs
@@ -4,11 +4,11 @@ pub mod zoo {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("client connecting");
+    println!("client monkey connecting");
     let mut client = zoo::monkey_client::MonkeyClient::connect("http://[::1]:8000").await?;
-    println!("client requesting");
+    println!("client monkey hello");
     println!(
-        "client {:?}",
+        "client monkey {:?}",
         client
             .hello(tonic::Request::new(zoo::Name {
                 first: "Clarence".into(),
@@ -16,5 +16,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }))
             .await?,
     );
+    println!("client bird connecting");
+    let mut client = zoo::bird_client::BirdClient::connect("http://[::1]:8000").await?;
+    println!("client bird listen");
+    let mut stream = client
+        .listen(tonic::Request::new(zoo::None {}))
+        .await?
+        .into_inner();
+    println!("client bird streaming");
+    for _ in 0..5 {
+        println!("client bird {:?}", stream.message().await?);
+    }
     Ok(())
 }
--- interfaces/grpc/rs-server/src/main.rs
+++ interfaces/grpc/rs-server/src/main.rs
@@ -1,3 +1,4 @@
+use tokio::sync::mpsc::{channel, Receiver};
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod zoo {
@@ -10,11 +11,47 @@ pub struct Monkey {}
 #[tonic::async_trait]
 impl zoo::monkey_server::Monkey for Monkey {
     async fn hello(&self, req: Request<zoo::Name>) -> Result<Response<zoo::Msg>, Status> {
-        println!("server {:?}", req);
+        println!("server monkey {:?}", req);
         let name = req.into_inner();
         Ok(Response::new(zoo::Msg {
             msg: format!("Ooh ooh ah {} ah!", name.first),
         }))
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Bird {}
+
+#[tonic::async_trait]
+impl zoo::bird_server::Bird for Bird {
+    type ListenStream = Receiver<Result<zoo::Msg, Status>>;//this is necessary
+
+    async fn listen(&self, req: Request<zoo::None>) -> Result<Response<Self::ListenStream>, Status> {
+        println!("server bird {:?}", req);
+        let (mut tx, rx) = channel(1);
+        tokio::spawn(async move {
+            let mut bird_brain = 0x1834;
+            loop {
+                println!("server bird sleeping");
+                tokio::time::delay_for(std::time::Duration::from_secs(2)).await;
+                println!("server bird awake");
+                tx.send(Ok(zoo::Msg {
+                    msg: match bird_brain % 4 {
+                        0 => "IT IS RAINING",
+                        1 => "THIS IS MY TREE",
+                        2 => "WHERE THE PRETTY BIRDS AT",
+                        3 => "THERE IS A BAD BIRD OR LAND LOVER",
+                        _ => "SQUAWK",
+                    }.into(),
+                })).await.unwrap();
+                bird_brain = (bird_brain >> 1) | if (bird_brain & 0x1) ^ (bird_brain & 0x40) != 0 {
+                    0x8000
+                } else {
+                    0
+                };
+            }
+        });
+        Ok(Response::new(rx))
     }
 }
 
@@ -23,6 +60,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("server starting");
     Server::builder()
         .add_service(zoo::monkey_server::MonkeyServer::new(Monkey::default()))
+        .add_service(zoo::bird_server::BirdServer::new(Bird::default()))
         .serve("[::1]:8000".parse()?)
         .await?;
     Ok(())
