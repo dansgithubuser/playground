77b6fb4339bcb06d0a9c694a4b81da362c3a2d81
Dan <daniel@x-matik.com>
2020-07-16 11:05:36 -0400

    serde_json object iter

--- languages/rust/serde_json/src/main.rs
+++ languages/rust/serde_json/src/main.rs
@@ -16,4 +16,20 @@ struct Array {
 fn main() {
     println!("{}", json!(Array { arr: vec![Element { a: None }] }).to_string());
     println!("{:?}", serde_json::from_str::<Array>(r#"{"arr": [{}]}"#));
+
+    println!("");
+
+    let val = json!({
+        "a": 1,
+        "b": 2,
+    });
+    let obj = val.as_object().unwrap();
+
+    obj.iter().map(|i| println!("{}: {}", i.0, i.1)).for_each(drop);
+
+    println!("");
+
+    for i in obj {
+        println!("{}: {}", i.0, i.1);
+    }
 }
