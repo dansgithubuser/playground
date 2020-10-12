1139dd05e6bee3786337626ada36c03b936b0388
Dan <dansonlinepresence@gmail.com>
2020-10-12 13:40:47 -0400

    (rust) mixin-play

---
+++ languages/rust/mixin-play/src/main.rs
@@ -0,0 +1,24 @@
+#[mixin::declare]
+pub struct Named {
+    name: String,
+}
+
+#[mixin::expand]
+impl Named {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+#[mixin::insert(Named)]
+pub struct MyStruct {}
+
+#[test]
+fn test_it() {
+    let my_struct = MyStruct { name: "MixIn Works" };
+    assert_eq!(my_struct.name(), "MixIn Works");
+}
+
+fn main() {
+    println!("Hello, world!");
+}
