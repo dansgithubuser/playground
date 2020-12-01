b51cd281800512678a4b2736cf0397b6dc783625
Dan <daniel@x-matik.com>
2020-12-01 13:36:58 -0500

    rusqlite, prevent db-is-locked

--- languages/rust/rusqlite/src/main.rs
+++ languages/rust/rusqlite/src/main.rs
@@ -2,22 +2,29 @@ fn main() {
     let args: Vec<_> = std::env::args().collect();
     let name = &args[1];
     println!("{}: started", name);
-    let conn = rusqlite::Connection::open_with_flags(
-        "lol.sql",
-        rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
-    )
-    .unwrap();
-    conn.busy_timeout(std::time::Duration::from_secs_f32(3.0)).unwrap();
-    conn.execute_batch(
-        "CREATE TABLE IF NOT EXISTS vars (
-            name TEXT PRIMARY KEY,
-            value TEXT NOT NULL,
-            metadata TEXT
-        )",
-    )
-    .expect("common DB schema execution failed");
-    println!("{}: opened connection", name);
-    for i in 0..1000 {
+    {
+        let conn = rusqlite::Connection::open_with_flags(
+            "lol.sql",
+            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE | rusqlite::OpenFlags::SQLITE_OPEN_FULL_MUTEX,
+        )
+        .unwrap();
+        conn.busy_timeout(std::time::Duration::from_secs_f32(3.0)).unwrap();
+        conn.execute_batch(
+            "CREATE TABLE IF NOT EXISTS vars (
+                name TEXT PRIMARY KEY,
+                value TEXT NOT NULL,
+                metadata TEXT
+            )",
+        )
+        .expect("common DB schema execution failed");
+    }
+    for i in 0..100 {
+        let conn = rusqlite::Connection::open_with_flags(
+            "lol.sql",
+            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE | rusqlite::OpenFlags::SQLITE_OPEN_FULL_MUTEX,
+        )
+        .unwrap();
+        conn.busy_timeout(std::time::Duration::from_secs_f32(3.0)).unwrap();
         conn.execute_batch(&format!("INSERT OR IGNORE INTO vars VALUES ('{}', '{}', NULL)", i, i))
             .unwrap();
         conn.query_row::<String, _, _>(
@@ -32,8 +39,10 @@ fn main() {
             i * 2
         ))
         .unwrap();
-        if i % 100 == 0 {
+        conn.close().unwrap();
+        if i % 10 == 0 {
             println!("{}: i = {}", name, i);
         }
+        std::thread::sleep(std::time::Duration::from_secs_f32(0.01));
     }
 }
