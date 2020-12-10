4b46ed4bdc89c5df19dd3690b650585f32d57000
Dan <daniel@x-matik.com>
2020-12-10 18:46:04 -0500

    rust chrono come on man

--- languages/rust/chrono/src/main.rs
+++ languages/rust/chrono/src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("{}", chrono::Utc::now());
     println!("{}", chrono::Local::now());
     println!("{}", chrono::DateTime::parse_from_rfc3339("2020-07-31T23:59:59.000Z").unwrap());
+    println!("{:?}", chrono::NaiveDateTime::parse_from_str("20200101000000hahaha", "%Y%m%d%H%M%S"));
 }
