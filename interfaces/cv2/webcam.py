79de8c4d270cee4b57be12a01335e1d7a56916d6
Dan <dansonlinepresence@gmail.com>
2020-12-13 14:05:45 -0500

    (cv2) attempt at recording from webcam

--- interfaces/cv2/webcam.py
+++ interfaces/cv2/webcam.py
@@ -4,14 +4,25 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('camera_index', nargs='?', default=0, type=int)
+parser.add_argument('--file', action='store_true')
 args = parser.parse_args()
 
 cap = cv2.VideoCapture(args.camera_index)
+if args.file:
+    cod = cv2.VideoWriter_fourcc(*'H264')
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    ret, frame = cap.read()
+    writer = cv2.VideoWriter('rec.mp4', cod, fps, frame.shape[:2])
 
 while True:
     ret, frame = cap.read()
     cv2.imshow('Input', frame)
 
+    if args.file:
+        writer.write(frame)
+
     c = cv2.waitKey(1)
     if c == 27:
         break
+
+writer.release()
