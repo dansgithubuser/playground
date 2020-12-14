ba314cd2eafa83115453d5f3746edffe0cd331b3
Dan <daniel@x-matik.com>
2020-12-14 11:16:57 -0500

    (rust) panic threading/process exit

--- languages/rust/panic-hook/src/main.rs
+++ languages/rust/panic-hook/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     println!("{:?}", std::panic::set_hook(Box::new(|info| {
         println!("panic");
         println!("info: {}", info);
+        std::thread::spawn(|| {
+            std::thread::sleep(std::time::Duration::from_secs(2));
+            std::process::exit(2);
+        });
     })));
     panic!("lol");
 }
