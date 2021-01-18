df79c51b017edf90a9ac9a093875c63641da208c
Dan <daniel@x-matik.com>
2021-01-18 13:38:32 -0500

    rusqlite more nuance

--- languages/rust/rusqlite/src/main.rs
+++ languages/rust/rusqlite/src/main.rs
@@ -1,48 +1,68 @@
+use std::thread;
+use std::time::{Instant, Duration};
+
+fn get_conn() -> Result<rusqlite::Connection, rusqlite::Error> {
+    use rusqlite::OpenFlags;
+    let conn = rusqlite::Connection::open_with_flags(
+        "lol.sqlite",
+        OpenFlags::SQLITE_OPEN_READ_WRITE
+            | OpenFlags::SQLITE_OPEN_CREATE
+            | OpenFlags::SQLITE_OPEN_FULL_MUTEX,
+    )?;
+    conn.busy_timeout(Duration::from_secs_f32(0.1)).unwrap();
+    conn.execute_batch(
+        "CREATE TABLE IF NOT EXISTS vars (
+            name TEXT PRIMARY KEY,
+            value TEXT NOT NULL,
+            metadata TEXT
+        )",
+    )?;
+    Ok(conn)
+}
+
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     let name = &args[1];
     println!("{}: started", name);
-    {
-        let conn = rusqlite::Connection::open_with_flags(
-            "lol.sqlite",
-            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE | rusqlite::OpenFlags::SQLITE_OPEN_FULL_MUTEX,
-        )
-        .unwrap();
-        conn.busy_timeout(std::time::Duration::from_secs_f32(3.0)).unwrap();
-        conn.execute_batch(
-            "CREATE TABLE IF NOT EXISTS vars (
-                name TEXT PRIMARY KEY,
-                value TEXT NOT NULL,
-                metadata TEXT
-            )",
-        )
-        .expect("schema execution failed");
-    }
     for i in 0..100 {
-        let conn = rusqlite::Connection::open_with_flags(
-            "lol.sqlite",
-            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE | rusqlite::OpenFlags::SQLITE_OPEN_FULL_MUTEX,
-        )
-        .unwrap();
-        conn.busy_timeout(std::time::Duration::from_secs_f32(3.0)).unwrap();
-        conn.execute_batch(&format!("INSERT OR IGNORE INTO vars VALUES ('{}', '{}', NULL)", i, i))
-            .unwrap();
-        conn.query_row::<String, _, _>(
-            &format!("SELECT * FROM vars WHERE name = '{}'", i),
-            rusqlite::NO_PARAMS,
-            |row| row.get(0),
-        )
-        .unwrap();
-        conn.execute_batch(&format!(
-            "INSERT OR REPLACE INTO vars VALUES ('{}', '{}', NULL)",
-            i,
-            i * 2
-        ))
-        .unwrap();
+        let start = Instant::now();
+        let conn = match get_conn() {
+            Ok(conn) => conn,
+            Err(e) => {
+                println!("error when getting conn: {}, t = {} s", e, (Instant::now() - start).as_secs_f32());
+                thread::sleep(Duration::from_secs_f32(0.01));
+                continue;
+            }
+        };
+        if let Err(e) = (|| -> Result<(), rusqlite::Error> {
+            match i % 3 {
+                0 => conn.execute_batch(&format!("INSERT OR IGNORE INTO vars VALUES ('{}', '0', NULL)", i))?,
+                1 => {
+                    conn.execute_batch(&format!(
+                        "
+                        UPDATE vars
+                        SET value = CAST(CAST(value AS INTEGER) + 1 AS TEXT)
+                        WHERE name = '{}'
+                        ",
+                        i - 1
+                    ))?;
+                    thread::sleep(Duration::from_secs_f32(0.01));
+                }
+                2 => {
+                    conn.query_row::<String, _, _>(
+                        &format!("SELECT * FROM vars WHERE name = '{}'", i),
+                        rusqlite::NO_PARAMS,
+                        |row| row.get(0),
+                    )?;
+                    thread::sleep(Duration::from_secs_f32(0.01));
+                }
+                _ => panic!("bad math"),
+            }
+            Ok(())
+        })() {
+            println!("error when querying: {}, t = {} s", e, (Instant::now() - start).as_secs_f32());
+        };
         conn.close().unwrap();
-        if i % 10 == 0 {
-            println!("{}: i = {}", name, i);
-        }
-        std::thread::sleep(std::time::Duration::from_secs_f32(0.01));
+        println!("{}: i = {}, t = {}", name, i, (Instant::now() - start).as_secs_f32());
     }
 }
