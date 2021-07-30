b3be7199fdb90536defed7e8713749862f0b1378
Dan <daniel@x-matik.com>
2021-07-30 13:33:55 -0400

    ...unless different processes

--- languages/python/fcntl-lock.py
+++ languages/python/fcntl-lock.py
@@ -1,21 +1,25 @@
+import argparse
 import fcntl
 import random
-import threading
+import subprocess
 import time
 
-def thread_main(thread_i):
+parser = argparse.ArgumentParser()
+parser.add_argument('id', nargs='?', default='main')
+args = parser.parse_args()
+
+if args.id == 'main':
+    procs = []
+    for i in range(10):
+        p = subprocess.Popen(['python3', 'fcntl-lock.py', str(i)])
+        procs.append(p)
+    for p in procs:
+        p.wait()
+else:
     for i in range(10):
         lock = open('lock', 'w')
         fcntl.lockf(lock, fcntl.LOCK_EX)
-        print(f'thread {thread_i}, iteration {i}, locked')
+        print(f'process {args.id}, iteration {i}, locked')
         time.sleep(random.random())
-        print(f'thread {thread_i}, iteration {i}, unlocking')
+        print(f'process {args.id}, iteration {i}, unlocking')
         lock.close()
-
-threads = []
-for i in range(10):
-    thread = threading.Thread(target=thread_main, args=(i,))
-    threads.append(thread)
-    thread.start()
-for thread in threads:
-    thread.join()
