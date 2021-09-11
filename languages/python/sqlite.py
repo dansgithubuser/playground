8e1934914ef16e24758305d6da903fb8de00c7e7
Dan <dansonlinepresence@gmail.com>
2021-09-11 12:19:34 -0400

    python sqlite3 jsonb

--- languages/python/sqlite.py
+++ languages/python/sqlite.py
@@ -5,10 +5,12 @@ if os.path.exists('db.sqlite3'): os.remove('db.sqlite3')
 
 conn = sqlite3.connect('db.sqlite3')
 
-conn.execute('CREATE TABLE rows (col1 INTEGER PRIMARY KEY AUTOINCREMENT, col2 TEXT)')
+conn.execute('CREATE TABLE rows (col1 INTEGER PRIMARY KEY AUTOINCREMENT, col2 TEXT, col3 JSONB)')
 conn.execute("INSERT INTO rows (col2) VALUES ('hello')")
 conn.execute("INSERT INTO rows (col2) VALUES ('goodbye')")
+conn.execute('''INSERT INTO rows (col3) VALUES ('{"a": 1}')''')
 conn.commit()
 
 print(conn.execute('SELECT * FROM rows').fetchall())
 print(conn.execute('SELECT col2 FROM rows WHERE col1 = 1').fetchone())
+print(conn.execute('SELECT col3 FROM rows WHERE col1 = 3').fetchone())
