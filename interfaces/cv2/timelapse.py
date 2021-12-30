7e07fa868b7c348beebd53be0c37995abd0d79c1
Dan <dansonlinepresence@gmail.com>
2021-12-30 11:36:46 -0500

    timelapse exception reliability

--- interfaces/cv2/timelapse.py
+++ interfaces/cv2/timelapse.py
@@ -12,20 +12,26 @@ parser.add_argument('--height', type=int)
 parser.add_argument('--extension', '-e', default='png')
 args = parser.parse_args()
 
-cap = cv2.VideoCapture(args.camera_index)
-
-if args.width: cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.width)
-if args.height: cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.height)
+def main():
+    cap = cv2.VideoCapture(args.camera_index)
+    if args.width: cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.width)
+    if args.height: cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.height)
+    while True:
+        ret, frame = cap.read()
+        file_name = (
+            '{:%Y-%m-%b-%d_%H-%M-%S}.{}'
+            .format(
+                datetime.datetime.now(),
+                args.extension,
+            )
+            .lower()
+        )
+        cv2.imwrite(file_name, frame)
+        time.sleep(args.period)
 
 while True:
-    ret, frame = cap.read()
-    file_name = (
-        '{:%Y-%m-%b-%d_%H-%M-%S}.{}'
-        .format(
-            datetime.datetime.now(),
-            args.extension,
-        )
-        .lower()
-    )
-    cv2.imwrite(file_name, frame)
-    time.sleep(args.period)
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    time.sleep(1)
