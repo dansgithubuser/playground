328aa61f32a62d4296c7ae4296e9cff12957de98
Dan <daniel@foresightanalytics.ca>
2023-04-06 14:05:57 -0400

    more timestamp

--- languages/python/timestamp.py
+++ languages/python/timestamp.py
@@ -9,4 +9,7 @@ def timestamp_utc():
 def timestamp_file():
     return datetime.utcnow().isoformat('_', 'seconds').replace(':', '-') + 'Z'
 
+def timestamp_file_non_utc():
+    return datetime.now().astimezone().strftime('%Y-%m-%d_%H-%M-%S_%Z')
+
 if __name__ == '__main__': print(timestamp())
