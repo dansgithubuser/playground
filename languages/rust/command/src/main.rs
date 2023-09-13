6d1a2f3204f6dbaeb7193a4b238e05ce5579345d
Dan <daniel@foresightanalytics.ca>
2023-09-13 15:39:40 -0400

    rust Command stdin piped

--- languages/rust/command/src/main.rs
+++ languages/rust/command/src/main.rs
@@ -1,5 +1,16 @@
+use std::io::Write;
+use std::process::{Command, Stdio};
+
 fn main() {
-    let mut proc = std::process::Command::new("python3");
+    let child = Command::new("cat")
+        .stdin(Stdio::piped())
+        .spawn()
+        .unwrap();
+    child.stdin.as_ref().unwrap().write_all(b"Hello, cat!\n").unwrap();
+    child.stdin.as_ref().unwrap().write_all(b"Meow.\n").unwrap();
+    drop(child);
+
+    let mut proc = Command::new("python3");
     proc.args(["-c", "import time; time.sleep(5); print('Hello from child!')"]);
     let child = proc.spawn().unwrap();
     drop(child);
