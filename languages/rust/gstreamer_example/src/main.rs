aa1e62d5dda0eaabf61f6a84daa7ce0653de71ee
Dan <daniel@foresightanalytics.ca>
2023-11-03 17:12:49 -0400

    play

--- languages/rust/gstreamer_example/src/main.rs
+++ languages/rust/gstreamer_example/src/main.rs
@@ -4,7 +4,12 @@
 - https://github.com/sdroege/gstreamer-rs/blob/main/examples/src/bin/appsink.rs
 */
 
-use gstreamer::prelude::{ElementExtManual, GstBinExt};
+use gstreamer::prelude::{
+    ElementExt,
+    ElementExtManual,
+    GstBinExt,
+    GstObjectExt,
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     gstreamer::init()?;
@@ -15,5 +20,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     pipeline.add(&cam)?;
     pipeline.add(&appsink)?;
     cam.link(&appsink)?;
+    pipeline.set_state(gstreamer::State::Playing)?;
+    let bus = pipeline.bus().unwrap(); // pipeline without bus shouldn't happen
+    for msg in bus.iter_timed(gstreamer::ClockTime::NONE) { // don't timeout
+        match msg.view() {
+            gstreamer::MessageView::Eos(..) => break,
+            gstreamer::MessageView::Error(err) => {
+                eprintln!(
+                    "Received error from {}: {} (debug: {:?})",
+                    msg
+                        .src()
+                        .map(|src| src.path_string().to_string())
+                        .unwrap_or("UNKNOWN".into()),
+                    err.error(),
+                    err.debug(),
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+    pipeline.set_state(gstreamer::State::Null)?;
     Ok(())
 }
