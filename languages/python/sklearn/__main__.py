975303e68ea95468dcc7164e14fed39c3442b200
Dan <daniel@foresightanalytics.ca>
2023-12-19 16:24:30 -0500

    svm importance

--- languages/python/sklearn/__main__.py
+++ languages/python/sklearn/__main__.py
@@ -1,15 +1,25 @@
 from sklearn import svm
+from sklearn.inspection import permutation_importance
 
 import random
 
-def make_sample(noise=1, dims=10):
-    return [random.uniform(0, noise) for _ in range(dims)]
+def make_sample(noise=[1], dims=10):
+    result = []
+    for i in range(dims):
+        if i >= len(noise):
+            m = noise[-1]
+        else:
+            m = noise[i]
+        result.append(random.uniform(0, m))
+    return result
 
-relevant = [make_sample(2) for i in range(100)]
-irrelevant = [make_sample(1) for i in range(1000)]
+relevant = [make_sample([5, 1, 2]) for i in range(100)]
+irrelevant = [make_sample([2, 2, 1]) for i in range(1000)]
+features = relevant + irrelevant
+labels = [1] * len(relevant) + [0] * len(irrelevant)
 
 classifier = svm.SVC()
-classifier.fit(relevant + irrelevant, [1] * len(relevant) + [0] * len(irrelevant))
+classifier.fit(features, labels)
 
 true_positive = 0
 false_negative = 0
@@ -25,7 +35,13 @@ for label in classifier.predict(irrelevant):
         true_negative += 1
     else:
         false_positive += 1
+print('RESULT QUALITY')
 print('true positive', true_positive)
 print('false negative', false_negative)
 print('true negative', true_negative)
 print('false positive', false_positive)
+print()
+print('FEATURE IMPORTANCE')
+importance = permutation_importance(classifier, features, labels)
+for i in reversed(importance.importances_mean.argsort()):
+    print(f'{i} {importance.importances_mean[i]:.5f}')
