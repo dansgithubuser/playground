b6ec03375c74cb48220022e700364f8eb98987ab
Dan <daniel@foresightanalytics.ca>
2024-03-06 13:39:51 -0500

    bitrate 640 kbps

--- interfaces/gstreamer/rec-rpi.sh
+++ interfaces/gstreamer/rec-rpi.sh
@@ -1,1 +1,7 @@
-gst-launch-1.0 libcamerasrc ! queue ! v4l2convert ! v4l2h264enc ! video/x-h264,level='(string)4' ! h264parse ! queue ! mpegtsmux ! filesink location=test.ts
+gst-launch-1.0 -e libcamerasrc ! queue \
+! v4l2convert \
+! v4l2h264enc extra-controls=encode,video_bitrate=640000 ! video/x-h264,level='(string)4' \
+! h264parse ! queue \
+! mpegtsmux ! filesink location=test.ts
+
+# check v4l2-ctl -d 11 --list-ctrls-menu for what to put in h264 encode extra-controls
