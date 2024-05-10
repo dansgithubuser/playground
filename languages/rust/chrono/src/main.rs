b7727bc4d38c9d6a183c6b19c3658378632f783c
Dan <daniel@foresightanalytics.ca>
2024-05-10 11:38:36 -0400

    (rust) more chrono

--- languages/rust/chrono/src/main.rs
+++ languages/rust/chrono/src/main.rs
@@ -7,5 +7,21 @@ fn main() {
     println!("{}", chrono::Local::now());
     println!("{}", chrono::DateTime::parse_from_rfc3339("2020-07-31T23:59:59.000Z").unwrap());
     println!("{:?}", chrono::NaiveDateTime::parse_from_str("20200101000000hahaha", "%Y%m%d%H%M%S"));
+    println!("{:?}", chrono::DateTime::parse_from_rfc3339("2020-07-31"));
+    println!("{:?}", chrono::DateTime::parse_from_rfc3339("2020-07-31Z"));
+    println!("{:?}", chrono::DateTime::parse_from_rfc2822("2020-07-31"));
+    println!("{:?}", chrono::DateTime::parse_from_rfc2822("2020-07-31Z"));
+    {
+        use chrono::TimeZone;
+        println!(
+            "{}",
+            chrono::Utc.from_utc_datetime(
+                &chrono::NaiveDate::parse_from_str("2020-07-31", "%Y-%m-%d")
+                    .unwrap()
+                    .and_hms_opt(0, 0, 0)
+                    .unwrap(),
+            ),
+        );
+    }
     println!("{}", timestamp_file());
 }
