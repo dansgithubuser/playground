682c4a8f8aded753042535256a1b2c48423e1de4
Dan <daniel@foresightanalytics.ca>
2024-07-17 14:21:11 -0400

    python sqlite3 follow docs re conn vs con

--- languages/python/sqlite.py
+++ languages/python/sqlite.py
@@ -3,14 +3,14 @@ import sqlite3
 
 if os.path.exists('db.sqlite3'): os.remove('db.sqlite3')
 
-conn = sqlite3.connect('db.sqlite3')
+con = sqlite3.connect('db.sqlite3')
 
-conn.execute('CREATE TABLE rows (col1 INTEGER PRIMARY KEY AUTOINCREMENT, col2 TEXT, col3 JSONB)')
-conn.execute("INSERT INTO rows (col2) VALUES ('hello')")
-conn.execute("INSERT INTO rows (col2) VALUES ('goodbye')")
-conn.execute('''INSERT INTO rows (col3) VALUES ('{"a": 1}')''')
-conn.commit()
+con.execute('CREATE TABLE rows (col1 INTEGER PRIMARY KEY AUTOINCREMENT, col2 TEXT, col3 JSONB)')
+con.execute("INSERT INTO rows (col2) VALUES ('hello')")
+con.execute("INSERT INTO rows (col2) VALUES ('goodbye')")
+con.execute('''INSERT INTO rows (col3) VALUES ('{"a": 1}')''')
+con.commit()
 
-print(conn.execute('SELECT * FROM rows').fetchall())
-print(conn.execute('SELECT col2 FROM rows WHERE col1 = 1').fetchone())
-print(conn.execute('SELECT col3 FROM rows WHERE col1 = 3').fetchone())
+print(con.execute('SELECT * FROM rows').fetchall())
+print(con.execute('SELECT col2 FROM rows WHERE col1 = 1').fetchone())
+print(con.execute('SELECT col3 FROM rows WHERE col1 = 3').fetchone())
