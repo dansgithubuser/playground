f283fd3ed63b64788a27dc2766c62d6fd331a77c
Dan <daniel@foresightanalytics.ca>
2024-07-17 11:09:04 -0400

    consolidate pyglet

--- interfaces/pyglet/texture.py
+++ interfaces/pyglet/texture.py
@@ -73,7 +73,7 @@ def update(dummy):
         square1.angle -= 5
 
 
-square1 = TexturedSquare(120, 120, 300, 200, '../../../gull.jpg')
+square1 = TexturedSquare(120, 120, 300, 200, '../../gull.jpg')
 
 pyglet.clock.schedule_interval(update,1/60.0)
 pyglet.app.run()
