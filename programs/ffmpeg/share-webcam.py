11fe823061ff84ac3c071868180c600214554443
Dan <dansonlinepresence@gmail.com>
2025-01-02 17:23:25 -0500

    share-webcam.py specify camera

--- programs/ffmpeg/share-webcam.py
+++ programs/ffmpeg/share-webcam.py
@@ -6,10 +6,11 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser()
+parser.add_argument('camera_index', default='0')
 parser.add_argument('server', nargs='?', default='localhost')
 args = parser.parse_args()
 
 def invoke(invocation):
     subprocess.run(invocation, shell=True, check=True)
 
-invoke(f'ffmpeg -f v4l2 -framerate 30 -video_size 640x480 -i /dev/video0 -f rtsp rtsp://{args.server}:8554/webcam')
+invoke(f'ffmpeg -f v4l2 -framerate 30 -video_size 640x480 -i /dev/video{args.camera_index} -f rtsp rtsp://{args.server}:8554/webcam')
