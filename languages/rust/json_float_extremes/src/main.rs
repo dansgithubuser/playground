b06e98542f21c4faceae9741b11ea66e031a316a
Daniel Eisner <daniel.eisner@xanadu.ai>
2025-02-19 12:48:23 -0500

    rust json float extremes

---
+++ languages/rust/json_float_extremes/src/main.rs
@@ -0,0 +1,13 @@
+use serde_json::{json, Value};
+
+fn main() {
+    println!("{:?}", json!(f64::MIN));
+    println!("{:?}", json!(f64::MAX));
+    println!("{:?}", json!(f64::MIN).to_string());
+    println!("{:?}", json!(f64::MAX).to_string());
+    println!("{:?}", serde_json::from_str::<Value>(&json!(f64::MIN).to_string()));
+    println!("{:?}", serde_json::from_str::<Value>(&json!(f64::MAX).to_string()));
+    println!("{:?}", serde_json::from_str::<Value>(&json!(f64::MIN).to_string()).unwrap().as_f64().unwrap());
+    println!("{:?}", serde_json::from_str::<Value>(&json!(f64::MAX).to_string()).unwrap().as_f64().unwrap());
+    println!("{:?}", serde_json::from_str::<Value>("1e1000"));
+}
