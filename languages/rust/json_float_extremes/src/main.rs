27c188646f51f1e6993610c6436146a9b9ec444f
Daniel Eisner <daniel.eisner@xanadu.ai>
2025-02-24 10:49:16 -0500

    serde_json can't do f64::MAX in unscientific notation

--- languages/rust/json_float_extremes/src/main.rs
+++ languages/rust/json_float_extremes/src/main.rs
@@ -10,4 +10,9 @@ fn main() {
     println!("{:?}", serde_json::from_str::<Value>(&json!(f64::MIN).to_string()).unwrap().as_f64().unwrap());
     println!("{:?}", serde_json::from_str::<Value>(&json!(f64::MAX).to_string()).unwrap().as_f64().unwrap());
     println!("{:?}", serde_json::from_str::<Value>("1e1000"));
+    {
+        let mut s = "17976931348623157".to_string();
+        s.push_str(&"0".repeat(308));
+        println!("{:?}", serde_json::from_str::<Value>(&s));
+    }
 }
